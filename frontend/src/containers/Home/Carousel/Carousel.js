9bf7d4c0e0e6f240a62bdf8e5c4205050d837924
lanxeon <lanxeon@users.noreply.github.com>
2020-10-18 21:25:04 +0530

    added carousel component

--- frontend/src/containers/Home/Carousel/Carousel.js
+++ frontend/src/containers/Home/Carousel/Carousel.js
@@ -1,21 +1,28 @@
 import React from "react";
 
-import { Carousel as LibraryCarousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 import classes from "./Carousel.module.css";
 
 const Carousel = (props) => {
 	return (
 		<div className={classes.Carousel}>
-			<LibraryCarousel>
-				<div style={{ overflow: "hidden" }}>
-					<img
-						src="https://archivedscreenshots.s3.ap-south-1.amazonaws.com/trump-continues-bizarre-appeals-to-suburban-women-1603004424531-dark.jpg"
-						alt=""
-					/>
+			<CarouselProvider naturalSlideWidth={100} naturalSlideHeight={100} totalSlides={1} isPlaying>
+				<Slider>
+					<Slide index={0}>
+						<Image
+							src="https://archivedscreenshots.s3.ap-south-1.amazonaws.com/trump-continues-bizarre-appeals-to-suburban-women-1603004424531-dark.jpg"
+							alt="lol"
+							className={classes.carouselImg}
+						/>
+					</Slide>
+				</Slider>
+				<div className={classes.ButtonsWrapper}>
+					<ButtonBack className={classes.btn}>←</ButtonBack>
+					<ButtonNext className={classes.btn}>→</ButtonNext>
 				</div>
-			</LibraryCarousel>
+			</CarouselProvider>
 		</div>
 	);
 };
