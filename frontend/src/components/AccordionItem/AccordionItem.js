7e199ae21ddd840afb79125d0940da82e03232a8
lanxeon <lanxeon@users.noreply.github.com>
2020-11-02 09:34:10 +0530

    added more font sizes for Raleway and removed some `Typography` tags

--- frontend/src/components/AccordionItem/AccordionItem.js
+++ frontend/src/components/AccordionItem/AccordionItem.js
@@ -7,30 +7,43 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-import { makeStyles } from "@material-ui/core/styles";
-const useStyles = makeStyles((theme) => ({
-	root: {
-		width: "100%",
-		backgroundColor: "var(--bg-content)",
-		color: "var(--text-color)",
-	},
-	heading: {
-		fontSize: theme.typography.pxToRem(15),
-		fontWeight: theme.typography.fontWeightRegular,
-	},
-}));
+// import { makeStyles } from "@material-ui/core/styles";
+// const useStyles = makeStyles((theme) => ({
+// 	root: {
+// 		width: "100%",
+// 		backgroundColor: "var(--bg-content)",
+// 		color: "var(--text-color)",
+// 	},
+// 	heading: {
+// 		fontSize: theme.typography.pxToRem(15),
+// 		fontWeight: theme.typography.fontWeightRegular,
+// 	},
+// 	svg: {
+// 		fill: "var(--text-color)",
+// 		color: "var(--text-color)",
+// 	},
+// }));
 
 const AccordionItem = (props) => {
-	const materialClasses = useStyles();
+	// const materialClasses = useStyles();
 	const { item } = props;
 
 	return (
 		<div className={classes.Wrapper}>
-			<Accordion className={classes.Accordion}>
-				<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content">
-					<Typography className={materialClasses.heading}>{item.headline}</Typography>
+			<Accordion className={classes.Accordion} TransitionProps={{ unmountOnExit: true }}>
+				<AccordionSummary
+					expandIcon={<ExpandMoreIcon style={{ color: "var(--text-color)" }} />}
+					aria-controls="headline/article content"
+				>
+					<h4 className={classes.heading}>{item.headline}</h4>
 				</AccordionSummary>
-				<AccordionDetails>
+				<AccordionDetails className={classes.AccordionContent}>
+					{item.type === "article" ? (
+						<p className={classes.authors}>
+							- By <span>{item.authors.join(", ")}</span>
+						</p>
+					) : null}
+
 					<div className={classes.imgContainer}>
 						<img src={item.screenshotLight} alt="lol" />
 					</div>
